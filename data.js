4dba397c1412f2d54a303f372b140fb418e8aa94
leepule <764444153@qq.com>
2018-11-22 16:55:06 +0800

    Update data.js

--- data.js
+++ data.js
@@ -20,3 +20,22 @@ Date.prototype.Format = function (fmt) {
 }
 
 new Date().Format('年份yyyy 月份MM 日期DD 小时hh 分钟mm 秒ss 季度q 毫秒S')
+
+
+if(newNumber.indexOf('.')!=-1&&newNumber.match(/\./g).length>=2){
+    //不能有两个小数点
+}else if(newNumber.length==1&&newNumber=="."){
+    //第一个数不能为小数点
+}else if(newNumber.length==2&&newNumber[0]=='0'&&newNumber[1]!='.'){
+    //第一位是0，第二位必须是小数点
+}else if(newNumber.indexOf('.')!=-1&&newNumber.split('.')[1].length>options.point){
+    //小数点之后的位数不能大于参数中的point
+}else if(parseFloat(newNumber)<0){
+    //不能为负数
+}else if(options.hasOwnProperty('max')&&parseFloat(newNumber)>options.max){
+    //最大值不能超过参数中的max
+}else if(!options.point&&newNumber.indexOf('.')!=-1){
+    //如果参数中的小数点之后的位数要求是0，则不能输入小数点
+}else{
+    //可以输入
+};
